2b8c4e82bdc9231c0fd3cdd51a5ca026d5b0943a
lodra24 <orhanbulbul@gmail.com>
2025-05-30 16:53:28 +0300

    anasayfa ilk tasarım yapıldı

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,23 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.js', // JavaScript dosyalarınızda da Tailwind sınıfları kullanıyorsanız ekleyin
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                // Varsayılan sans fontunu Inter olarak ayarla
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
             },
+            // İsterseniz özel renklerinizi veya diğer tema genişletmelerini buraya ekleyebilirsiniz
+            // Örneğin, TimeSkip yeşili veya MindMetrics marka renkleri
+            colors: {
+                'mindmetrics-green': '#10B981',
+                'mindmetrics-green-dark': '#059669',
+                'mindmetrics-indigo': '#4f46e5', // Vurgu için indigo
+                'mindmetrics-indigo-light': '#e0e7ff', // Butonlar için açık indigo
+            }
         },
     },
 
