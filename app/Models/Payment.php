c2112b2ef343b4b77ca04e71373ffd8c88db1052
lodra24 <orhanbulbul@gmail.com>
2025-05-30 13:05:23 +0300

    Payment Tablosu eklendi

---
+++ app/Models/Payment.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'transaction_id',
+        'amount',
+        'currency',
+        'status',
+        'payment_gateway',
+        'gateway_response',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'gateway_response' => 'array',
+    ];
+
+    /**
+     * Get the user that owns the payment.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
