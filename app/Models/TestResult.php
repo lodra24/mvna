9228c3b7cdedc9b6e5ebb2c80c72e0f5791688a7
lodra24 <orhanbulbul@gmail.com>
2025-06-18 20:27:58 +0300

    aynı calculateMbtiType() metodu TestResult modelinden silindi

--- app/Models/TestResult.php
+++ app/Models/TestResult.php
@@ -55,25 +55,4 @@ class TestResult extends Model
         return $this->hasOne(MbtiTypeDetail::class, 'mbti_type', 'mbti_type');
     }
 
-    /**
-     * MBTI tipini hesapla
-     */
-    public function calculateMbtiType()
-    {
-        $mbti = '';
-        
-        // E/I boyutu
-        $mbti .= $this->e_score > $this->i_score ? 'E' : 'I';
-        
-        // S/N boyutu
-        $mbti .= $this->s_score > $this->n_score ? 'S' : 'N';
-        
-        // T/F boyutu
-        $mbti .= $this->t_score > $this->f_score ? 'T' : 'F';
-        
-        // J/P boyutu
-        $mbti .= $this->j_score > $this->p_score ? 'J' : 'P';
-        
-        return $mbti;
-    }
 }
